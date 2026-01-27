35de6a6978a4c806a62f0ba3f68b7bf5bb123b9b
Nabin <nabindrashrestha10@gmail.com>
2026-01-27 18:23:56 -0500

    completed the read_excel_function

--- project1/power_system.py
+++ project1/power_system.py
@@ -21,3 +21,24 @@ def read_from_excel(path: str)->dict:
         - branch: DataFrame
             contains branch data
     """
+    file_dict = pd.read_excel("case14.xlsx", sheet_name = ['bus', 'branch', 'gen'], 
+                         header = 0, )
+    return file_dict
+
+if __name__ == "__main__":
+    data = read_from_excel("case14.xslx")
+    print(data)
+
+    # Access individual DataFrames
+    bus_df = data['bus']
+    gen_df = data['gen']
+    branch_df = data['branch']
+
+    # print first few rows of each DataFrame
+    print("\nBus Data:")
+    print(bus_df.head())
+    print("\nGenerator Data:")
+    print(gen_df.head())
+    print("\nBranch Data:")
+    print(branch_df.head())
+    print(f"\n Type of branch df: {type(branch_df)}")

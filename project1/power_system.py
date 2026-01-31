9434538c421e5348ebacb0226ed4295457a34ea4
nabinhere <nabindhashrestha10@gmail.com>
2026-01-31 14:35:45 -0500

    added a function to create json file from power system data

--- project1/power_system.py
+++ project1/power_system.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib as plt
+import json
 
 def read_from_excel(path: str)->dict:
     """
@@ -9,7 +10,6 @@ def read_from_excel(path: str)->dict:
     -----------
     path: str
         path to the Excel file
-    
     Returns
     --------
     dict
@@ -24,6 +24,28 @@ def read_from_excel(path: str)->dict:
     file_dict = pd.read_excel("case14.xlsx", sheet_name = None, 
                          header = 0)
     return file_dict
+
+def save_to_json(data: dict, path: str)->None:
+    """
+    Save data dictionary to json file
+
+    Parameters
+    -----------
+    data: dict
+        dictionary containing three pandas DataFrames: 
+            - bus, gen, and branch
+    
+    path: str
+        string containing the relative path to save the json file
+
+    """
+    data_dict = {}
+    for key in data.keys():
+        data_dict[key] = data[key].to_dict()
+    
+    with open(path, 'w') as json_file:
+        json_file.write(json.dumps(data_dict))
+
 
 
 if __name__ == "__main__":
@@ -41,3 +63,6 @@ if __name__ == "__main__":
     print(gen_df.head())
     print("\nBranch Data:")
     print(branch_df.head())
+
+    # Save data to a json file
+    save_to_json(data = data, path = "power_system_data.json")

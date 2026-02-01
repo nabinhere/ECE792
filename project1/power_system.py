20c25a0934cf557d63aa37f0eab3bd5a246cc570
nabinhere <nabindrashrestha10@gmail.com>
2026-02-01 14:39:15 -0500

    added function and codeblocks for visualization

--- project1/power_system.py
+++ project1/power_system.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import plotly.graph_objects as go
 import json
 from bus import Bus
 from generator import Generator
@@ -112,7 +113,35 @@ def column_to_obj(df, cls):
         obj_dict[col] = df[col].tolist()
     return cls(**obj_dict)
 
+def plot_bus_voltage(bus: object)->None:
+    plt.figure(figsize=(10, 6))
+    plt.plot(bus.bus_i, bus.Vm, 'b-o', label = 'Voltage Magnitude')
+    plt.xlabel('Bus Number')
+    plt.ylabel('Voltage Magnitude')
+    plt.title('Bus Voltage Magnitude Plot')
+    plt.legend('Vmag')
+    plt.grid()
 
+    # save the plot
+    plt.savefig('bus_voltage_magnitude_plot', dpi=400, format="png", bbox_inches="tight")
+
+    plt.show()
+
+def interactive_plot(bus: object):
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=bus.bus_i, y=bus.Vm, name="Voltage Magnitude"))
+
+    fig.update_layout(
+        title="Bus Voltage Profile",
+        xaxis_title="Bus Number",
+        yaxis_title="Voltage (p.u.)"
+    )  
+
+    # save as HTML file
+    fig.write_html("voltage_profile_interactive.html")
+    
+    
 if __name__ == "__main__":
     # read power system data from an excel file
     data = read_from_excel("case14.xslx")
@@ -133,6 +162,39 @@ if __name__ == "__main__":
     print(bus.Vm[0])
     print(branch.angmax[0])
     print(generator.Pmax[0])
+
+    # plot bus voltage
+    # plot_bus_voltage(bus)
+
+    # Plot with subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (12,5))
+
+    # voltage magnitude plot
+    ax1.plot(bus.bus_i, bus.Vm, 'r-o')
+    ax1.set_xlabel('Bus Number')
+    ax1.set_ylabel('Voltage Magnitude (p.u.)')
+    ax1.set_title('Bus Voltage Magnitude Plot')
+    ax1.grid()
+
+    # voltage angle plot
+    ax2.plot(bus.bus_i, bus.Va, 'r-o')
+    ax2.set_xlabel('Bus Number')
+    ax2.set_ylabel('Voltage Angle (deg)')
+    ax2.set_title('Bus Voltage Angle Plot')
+    ax2.grid()
+
+    fig.suptitle("Bus Voltage and Magnitude Plots")
+
+    plt.tight_layout()
+
+    # save the plot
+    plt.savefig('bus voltage and magnitude plot', dpi=400, format="png", bbox_inches="tight")
+
+    # plt.show()
+
+    interactive_plot(bus)
+
+
 
 
 

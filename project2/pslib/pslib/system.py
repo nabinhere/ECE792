e3507a64a3beaf69e7401f24b81223d953b2b617
Nabin <nabindrashrestha10@gmail.com>
2026-02-15 15:18:21 -0500

    vectoried the makeYbus function

--- project2/pslib/pslib/system.py
+++ project2/pslib/pslib/system.py
@@ -8,27 +8,29 @@ class PowerSystem:
     def makeYbus(self):
         fbus = self.bus.ext2int(self.branch.fbus)
         tbus = self.bus.ext2int(self.branch.tbus)
+        # convert to numpy arrays to enable vectorized operation
+        fbus = np.array(fbus)
+        tbus = np.array(tbus)
+        r = np.array(self.branch.r)
+        x = np.array(self.branch.x)
+        b = np.array(self.branch.b)
 
-        branches = [fbus, tbus, self.branch.r, self.branch.x, self.branch.b]
+        # branch impedance and admittance
+        z = r + 1j*x
+        y = 1 / z
 
-        n_bus = len(self.bus.getcount())     # number of branches
-        # initialize the Y bus matrix
-        Ybus = np.zeros((n_bus, n_bus))
-
-        for branch in branches:
-            f, t, r, x, b = branch
-
-            z = r + 1j*x
-            y = 1/z
-
-            # off-diagonal elements
-            Ybus[f][t] = -y
-            Ybus[t][f] = -y
-
-            # diagonal elements
-            Ybus[f][f] += y + 1j*b/2
-            Ybus[t][t] += y + 1j*b/2
+        n_bus = self.bus.get_count()    #number of buses
+        # initialize the Ybus matrix
+        Ybus = np.zeros((n_bus, n_bus), dtype=complex)
+        # off-diagonal elements
+        Ybus[fbus, tbus] = -y
+        Ybus[tbus, fbus] = -y
+        # diagonal elements
+        np.add.at(Ybus, (fbus, fbus), y + 1j*b/2)
+        np.add.at(Ybus, (tbus, tbus), y + 1j*b/2)
 
         self.Ybus = Ybus
+
+        return self.Ybus
 
         

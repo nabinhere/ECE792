bd00afae3592eacbedece1b3657aba4bf92a8832
Nabin <nabindrashrestha10@gmail.com>
2026-02-15 14:15:56 -0500

    added external to internal mapping function in bus.py

--- project2/pslib/pslib/models/bus.py
+++ project2/pslib/pslib/models/bus.py
@@ -1,3 +1,7 @@
+import numpy as np
+from pslib.parsers import create_system_from_data
+from pslib.parsers.excel import read_from_excel
+
 class Bus:
     def __init__(self, bus_i, type, Pd, Qd, Gs, Bs,
                  area, Vm, Va, baseKV, zone, Vmax, Vmin):
@@ -13,6 +17,24 @@ class Bus:
         self.zone = zone
         self.Vmax = Vmax
         self.Vmin = Vmin
+
+        self._int_map = {}
     
     def get_count(self):
         return len(self.bus_i)
+    
+    def ext2int(self, ext_numbers):
+        """
+        Convert an array of external bus numbers to internal bus numbers.
+        """
+        return np.vectorize(self._int_map.get)(ext_numbers)
+    
+    def make_int_map(self):
+        """
+        Make the map from external bus number to internal bus number.
+
+        This function should be called before adding all bus data
+        """
+        self._int_map = {}
+        for i, ext_number in enumerate(self.ext_numbers):
+            self._int_map[ext_number] = i

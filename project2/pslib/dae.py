4e6be2a21e12751eac8102235a5cc60d73e33913
Nabin <nabindrashrestha10@gmail.com>
2026-03-01 15:48:55 -0500

    updated dae.py to correct the addition of equation addresses

--- project2/pslib/dae.py
+++ project2/pslib/dae.py
@@ -9,8 +9,8 @@ class DAE:
         """
         register a specified type of equations for a given model 
         """
-        num_p_eqn = eqn_dict["P"]   # number of P equations to be registered
-        num_q_eqn = eqn_dict["Q"]   # number of Q equations to be registered
+        num_p_eqn = eqn_dict["P_balance"]   # number of P equations to be registered
+        num_q_eqn = eqn_dict["Q_balance"]   # number of Q equations to be registered
         P_balance_dict = {}
         Q_balance_dict = {}
 
@@ -21,11 +21,12 @@ class DAE:
             P_balance_dict[val] = P_balance_address[i]
             Q_balance_dict[val] = Q_balance_address[i]
 
-        self.eqn_address[model_name] = {var_type:
+        self.eqn_address.update({model_name:
+                                {var_type:
                                         {"P_balance": P_balance_dict,
                                         "Q_balance": Q_balance_dict
                                         }
-                                    }
+                                    }})
                                     
         self.m += num_p_eqn + num_q_eqn     # update the next available equation address
 
@@ -58,60 +59,3 @@ class DAE:
             eqn_address.append(self.eqn_address[model_name][var_type][var_name][val])
 
         return eqn_address
-
-
-# #################################################################################
-# #################################################################################
-class Generator:
-    def __init__(self):
-        self.eqn_address = {}   # probably used to store the address of residual equations that a generator affects 
-        self.eqn_residuals = {}  # probably used to store the residual contribution of a generator
-
-    def fetch_eqn_address(self, dae, system):
-        # get the internal bus number of the generator
-        bus_int = system.bus.ext2int(self.bus)
-        # get the P_balance and Q_balance equation addresses
-        P_address = dae.get_eqn_address("Bus", "Algeb", "P_balance", bus_int)
-        Q_address = dae.get_eqn_address("Bus", "Algeb", "Q_balance", bus_int)
-        self.eqn_address = {"P_balance": P_address,
-                            "Q_balance": Q_address}
-        
-    def residual(self, x):
-        self.eqn_residuals = {"P_balance": self.Pg,
-                              "Q_balance": self.Qg}
-        
-
-class Bus:
-    def register_equations(self, dae):
-        dae.register_eqn("Bus", "Algeb", 
-                         {"P": self.get_count(),
-                          "Q": self.get_count()}, self._int_map.values())
-        
-        dae.register_eqn("Bus", "Algeb", self._int_map.values)
-    
-    def fetch_equation_address(self, dae, system):
-        bus_int = self.ext2int(self.bus_i)
-        # get the P_balance and Q_balance equation addresses
-        P_address = dae.get_eqn_address("Bus", "Algeb", "P_balance", bus_int)
-        Q_address = dae.get_eqn_address("Bus", "Algeb", "Q_balance", bus_int)
-
-        self.eqn_address = {"P_balance": P_address,
-                            "Q_balance": Q_address}
-        
-class Branch:
-    def __init__(self):
-        self.eqn_address = {}
-        self.eqn_residuals = {}
-
-    def fetch_eqn_address(self, dae, system):
-        fbus_int = system.bus.ext2it(self.fbus)
-        tbus_int = system.bus.ext2int(self.tbus)
-
-        P_address_fbus  = dae.get_eqn_address("Branch", "Algeb", "P_balance", fbus_int)
-        P_address_tbus  = dae.get_eqn_address("Branch", "Algeb", "P_balance", tbus_int)
-        Q_address_fbus  = dae.get_eqn_address("Branch", "Algeb", "Q_balance", fbus_int)
-        Q_address_tbus  = dae.get_eqn_address("Branch", "Algeb", "Q_balance", tbus_int)
-        self.eqn_address = {"P_balance_fbus": P_address_fbus,
-                            "P_balance_tbus": P_address_tbus,
-                            "Q_balance_fbus": Q_address_fbus,
-                            "Q_balance_tbus": Q_address_tbus} 

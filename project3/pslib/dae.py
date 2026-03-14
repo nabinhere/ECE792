7f88945464b132ba185963d43e60ed788214a00f
Nabin <nabindrashrestha10@gmail.com>
2026-03-14 12:33:57 -0400

    added register var function

--- project3/pslib/dae.py
+++ project3/pslib/dae.py
@@ -3,9 +3,11 @@ import numpy as np
 class DAE:
     def __init__(self):
         self.m = 0  # next available address
+        self.nvar = 0
         self.eqn_address = {}
+        self.var_address = {}
 
-    def register_eqn(self, model_name: str, var_type: str, eqn_dict: dict, bus_int):
+    def register_eqn(self, model_name: str, eqn_type: str, eqn_dict: dict, bus_int):
         """
         register a specified type of equations for a given model 
         """
@@ -14,13 +16,13 @@ class DAE:
             self.eqn_address[model_name] = {}
         
         # create a specified type of equation for a given model
-        if var_type not in self.eqn_address[model_name]:
-            self.eqn_address[model_name][var_type] = {}
+        if eqn_type not in self.eqn_address[model_name]:
+            self.eqn_address[model_name][eqn_type] = {}
 
         # Assign equation addresses
         for eqn_name, num_eqn in eqn_dict.items():
-            if eqn_name not in self.eqn_address[model_name][var_type]:
-                self.eqn_address[model_name][var_type][eqn_name] = {}
+            if eqn_name not in self.eqn_address[model_name][eqn_type]:
+                self.eqn_address[model_name][eqn_type][eqn_name] = {}
             
             address_dict = {}
             eqn_address = range(self.m, self.m+num_eqn)
@@ -28,23 +30,49 @@ class DAE:
             for i, val in enumerate(bus_int):
                 address_dict[val] = eqn_address[i]
 
-            self.eqn_address[model_name][var_type][eqn_name].update(address_dict)
+            self.eqn_address[model_name][eqn_type][eqn_name].update(address_dict)
 
 
-    def get_eqn_address(self, model_name: str, var_type: str, var_name: str, bus_no):
+    def register_var(self, model_name: str, var_type: str, var_dict: dict, bus_int):
+        """
+        register a specified type of variable for a given model 
+        """
+        # initialize a variable dictionary for a given model
+        if model_name not in self.var_address:
+            self.var_address[model_name] = {}
+        
+        # create a specified type of variable for a given model
+        if var_type not in self.var_address[model_name]:
+            self.var_address[model_name][var_type] = {}
+
+        # Assign variable addresses
+        for var_name, num_var in var_dict.items():
+            if var_name not in self.var_address[model_name][var_type]:
+                self.var_address[model_name][var_type][var_name] = {}
+            
+            address_dict = {}
+            var_address = range(self.nvar, self.nvar+num_var)
+            self.m += num_var
+            for i, val in enumerate(bus_int):
+                address_dict[val] = var_address[i]
+
+            self.var_address[model_name][var_type][var_name].update(address_dict)
+
+
+    def get_eqn_address(self, model_name: str, var_type: str, eqn_name: str, bus_no):
         """Get the address of the equation that is initially registered by 
         a given model using a model, variable type, variable name, bus numbers
         
         Parameters
         -----------
         model_name: str
-            name of the power system model
+            name of the power system model (e.g., "Bus")
         
         var_type: str
             Type of variable. Can be either algebraic or differential
 
-        var_name: str
-            Variable name
+        eqn_name: str
+            Name of the equation (e.g., "P")
         
         bus_no: int or np.ndarray
             internal bus number associated witht the given model
@@ -55,6 +83,6 @@ class DAE:
         """
         eqn_address = []
         for val in bus_no:
-            eqn_address.append(self.eqn_address[model_name][var_type][var_name][val])
+            eqn_address.append(self.eqn_address[model_name][var_type][eqn_name][val])
 
         return eqn_address
